09d8e624975535ba95217c8a277184ad7870a482
cb0806151 <cb0806151@otc.edu>
2023-05-19 20:47:06 -0500

    wire in local storage to persist timer defaults on refresh

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,28 @@
 import { Box, Button, Text, Input, Checkbox, Flex } from '@chakra-ui/react'
-import { useRef, useState } from 'react'
+import { RefObject, useEffect, useRef, useState } from 'react'
 import useSound from 'use-sound'
 
 import { Header } from '~components/Header'
+import { ls } from '~utils/localStorage'
 
 import oldChurchBell from '/old-church-bell.wav'
+
+interface IDefaultDurations {
+    lectioDefaultDuration: number
+    meditatioDefaultDuration: number
+    oratioDefaultDuration: number
+    contemplatioDefaultDuration: number
+}
+
+const defaultDuration = 5
+
+const defaultDurations = {
+    lectioDefaultDuration: defaultDuration,
+    meditatioDefaultDuration: defaultDuration,
+    oratioDefaultDuration: defaultDuration,
+    contemplatioDefaultDuration: defaultDuration,
+}
+const LS_DEFAULT_DURATIONS_KEY = 'DEFAULT_DURATIONS'
 
 export const App = (): JSX.Element => {
     const [isTimerActive, setIsTimerActive] = useState(false)
@@ -15,28 +33,56 @@ export const App = (): JSX.Element => {
     const [playBell] = useSound(oldChurchBell)
     const [isLectioTimerDisabled, setIsLectioTimerDisabled] = useState(false)
     const [isContemplatioDisabled, setIsContemplatioDisabled] = useState(false)
-    const timers = useRef<number[]>([])
+    const [timerDefaults, setTimerDefaults] = useState<IDefaultDurations | undefined>()
+    const segmentTimerCounts = useRef<number[]>([])
+
+    const getDefaultDurations = () => JSON.parse(ls.get(LS_DEFAULT_DURATIONS_KEY) ?? JSON.stringify(defaultDurations))
+
+    const getTimerDefaults = () => {
+        const fetchedTimerDefaults = getDefaultDurations()
+        setTimerDefaults(fetchedTimerDefaults)
+    }
+
+    useEffect(() => {
+        getTimerDefaults()
+    }, [])
+
+    const updateDefaults = (key: string, element: RefObject<HTMLInputElement>) => {
+        const fetchedTimerDefaults = getDefaultDurations()
+        fetchedTimerDefaults[key] = element?.current?.value || defaultDuration
+        ls.set(LS_DEFAULT_DURATIONS_KEY, JSON.stringify(fetchedTimerDefaults))
+    }
 
     const getTimers = () => {
-        timers.current = []
-        if (!isLectioTimerDisabled) timers.current.push(Number(lectioTimerInput.current?.value || 5))
-        timers.current.push(Number(meditatioTimerInput.current?.value || 5))
-        timers.current.push(Number(oratioTimerInput.current?.value || 5))
-        if (!isContemplatioDisabled) timers.current.push(Number(contemplatioTimerInput.current?.value || 5))
+        segmentTimerCounts.current = []
+        if (!isLectioTimerDisabled) segmentTimerCounts.current.push(Number(lectioTimerInput.current?.value || 5))
+        segmentTimerCounts.current.push(Number(meditatioTimerInput.current?.value || 5))
+        segmentTimerCounts.current.push(Number(oratioTimerInput.current?.value || 5))
+        if (!isContemplatioDisabled) segmentTimerCounts.current.push(Number(contemplatioTimerInput.current?.value || 5))
     }
 
     const startTimers = () => {
         getTimers()
         let cumulativeTimerCount = 0
-        timers.current.forEach((timerCount, index) => {
+        segmentTimerCounts.current.forEach((timerCount, index) => {
             const timerMinutes = 1000 * 60 * timerCount
             setTimeout(() => {
                 playBell()
-                if (index + 1 === timers.current.length) setIsTimerActive(false)
+                if (index + 1 === segmentTimerCounts.current.length) setIsTimerActive(false)
             }, timerMinutes + cumulativeTimerCount)
             cumulativeTimerCount += timerMinutes
         })
         setIsTimerActive(true)
+    }
+
+    if (timerDefaults === undefined) {
+        return (
+            <Box p="2">
+                <Header title="Lectio Divina Timer">
+                    <></>
+                </Header>
+            </Box>
+        )
     }
 
     return (
@@ -51,9 +97,10 @@ export const App = (): JSX.Element => {
                                         Lectio (minutes)
                                     </Text>
                                     <Input
-                                        defaultValue={5}
+                                        defaultValue={timerDefaults.lectioDefaultDuration}
                                         placeholder={'Input the number of minutes'}
                                         ref={lectioTimerInput}
+                                        onChange={() => updateDefaults('lectioDefaultDuration', lectioTimerInput)}
                                     />
                                 </>
                             )}
@@ -68,17 +115,19 @@ export const App = (): JSX.Element => {
                                 Meditatio (minutes)
                             </Text>
                             <Input
-                                defaultValue={5}
+                                defaultValue={Number(timerDefaults.meditatioDefaultDuration)}
                                 placeholder={'Input the number of minutes'}
                                 ref={meditatioTimerInput}
+                                onChange={() => updateDefaults('meditatioDefaultDuration', meditatioTimerInput)}
                             />
                             <Text mb="8px" mt="16px">
                                 Oratio (minutes)
                             </Text>
                             <Input
-                                defaultValue={5}
+                                defaultValue={timerDefaults.oratioDefaultDuration}
                                 placeholder={'Input the number of minutes'}
                                 ref={oratioTimerInput}
+                                onChange={() => updateDefaults('oratioDefaultDuration', oratioTimerInput)}
                             />
                             {!isContemplatioDisabled && (
                                 <>
@@ -86,9 +135,12 @@ export const App = (): JSX.Element => {
                                         Contemplatio (minutes)
                                     </Text>
                                     <Input
-                                        defaultValue={5}
+                                        defaultValue={timerDefaults.contemplatioDefaultDuration}
                                         placeholder={'Input the number of minutes'}
                                         ref={contemplatioTimerInput}
+                                        onChange={() =>
+                                            updateDefaults('contemplatioDefaultDuration', contemplatioTimerInput)
+                                        }
                                     />
                                 </>
                             )}
